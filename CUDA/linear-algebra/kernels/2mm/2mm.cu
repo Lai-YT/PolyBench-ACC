9cae9cc2e5470ed3df4eb5b53fc06202bd2c551f
Lai-YT <381xvmvbib@gmail.com>
2026-02-20 13:16:24 +0800

    [2mm] Delinearize arrays

--- CUDA/linear-algebra/kernels/2mm/2mm.cu
+++ CUDA/linear-algebra/kernels/2mm/2mm.cu
@@ -101,35 +101,35 @@ void GPU_argv_init()
 }
 
 
-__global__ void mm2_kernel1(int ni, int nj, int nk, int nl, DATA_TYPE alpha, DATA_TYPE beta, DATA_TYPE *tmp, DATA_TYPE *A, DATA_TYPE *B)
+__global__ void mm2_kernel1(int ni, int nj, int nk, int nl, DATA_TYPE alpha, DATA_TYPE beta, DATA_TYPE POLYBENCH_2D(tmp, NI, NJ, ni, nj), DATA_TYPE POLYBENCH_2D(A, NI, NK, ni, nk), DATA_TYPE POLYBENCH_2D(B, NK, NJ, nk, nj))
 {
 	int j = blockIdx.x * blockDim.x + threadIdx.x;
 	int i = blockIdx.y * blockDim.y + threadIdx.y;
 
 	if ((i < _PB_NI) && (j < _PB_NJ))
 	{ 
-		tmp[i * NJ + j] = 0;
+		tmp[i][j] = 0;
 		int k;
 		for (k = 0; k < _PB_NK; k++)
 		{
-			tmp[i * NJ + j] += alpha * A[i * NK + k] * B[k * NJ + j];
+			tmp[i][j] += alpha * A[i][k] * B[k][j];
 		}
 	}
 }
 
 
-__global__ void mm2_kernel2(int ni, int nj, int nk, int nl, DATA_TYPE alpha, DATA_TYPE beta, DATA_TYPE *tmp, DATA_TYPE *C, DATA_TYPE *D)
+__global__ void mm2_kernel2(int ni, int nj, int nk, int nl, DATA_TYPE alpha, DATA_TYPE beta, DATA_TYPE POLYBENCH_2D(tmp, NI, NJ, ni, nj), DATA_TYPE POLYBENCH_2D(C, NL, NJ, nl, nj), DATA_TYPE POLYBENCH_2D(D, NI, NL, ni, nl))
 {
 	int j = blockIdx.x * blockDim.x + threadIdx.x;
 	int i = blockIdx.y * blockDim.y + threadIdx.y;
 
 	if ((i < _PB_NI) && (j < _PB_NL))
 	{ 
-		D[i * NL + j] *= beta;
+		D[i][j] *= beta;
 		int k;
 		for (k = 0; k < _PB_NJ; k++)
 		{
-			D[i * NL + j] += tmp[i * NJ + k] * C[k * NL + j];
+			D[i][j] += tmp[i][k] * C[k][j];
 		}
 	}
 }
@@ -194,11 +194,11 @@ void mm2Cuda(int ni, int nj, int nk, int nl, DATA_TYPE alpha, DATA_TYPE beta, DA
 	DATA_TYPE POLYBENCH_2D(A,NI,NK,ni,nk), DATA_TYPE POLYBENCH_2D(B,NK,NJ,nk,nj), DATA_TYPE POLYBENCH_2D(C,NL,NJ,nl,nj), 
 	DATA_TYPE POLYBENCH_2D(D,NI,NL,ni,nl), DATA_TYPE POLYBENCH_2D(D_outputFromGpu,NI,NL,ni,nl))
 {
-	DATA_TYPE *tmp_gpu;
-	DATA_TYPE *A_gpu;
-	DATA_TYPE *B_gpu;
-	DATA_TYPE *C_gpu;
-	DATA_TYPE *D_gpu;
+	DATA_TYPE (*tmp_gpu)[NJ];
+	DATA_TYPE (*A_gpu)[NK];
+	DATA_TYPE (*B_gpu)[NJ];
+	DATA_TYPE (*C_gpu)[NJ];
+	DATA_TYPE (*D_gpu)[NL];
 
 	cudaMalloc((void **)&tmp_gpu, sizeof(DATA_TYPE) * NI * NJ);
 	cudaMalloc((void **)&A_gpu, sizeof(DATA_TYPE) * NI * NK);
